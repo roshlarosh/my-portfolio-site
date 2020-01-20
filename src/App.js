e6bdcb715ef6d54e67b409c22348feef3061c920
Roshan Thaabith <roshlarosh@gmail.com>
2020-01-20 09:11:45 +0000

    add backdrop to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import SideDrawer from "./components/SideDrawer/SideDrawer";
+import Backdrop from "./components/Backdrop/Backdrop";
 
 function App() {
   return (
     <div style={{ height: "100%" }}>
       <Navbar />
       <SideDrawer />
+      <Backdrop />
       <main style={{ marginTop: 64 }}>
         <p>This is the page paragraph</p>
       </main>
