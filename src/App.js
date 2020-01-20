b569249c6f15fe18e29fbeabca9e944defc7cdc0
Roshan Thaabith <roshlarosh@gmail.com>
2020-01-20 09:03:25 +0000

    add side drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ height: "100%" }}>
       <Navbar />
+      <SideDrawer />
       <main style={{ marginTop: 64 }}>
         <p>This is the page paragraph</p>
       </main>
