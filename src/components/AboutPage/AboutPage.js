c5874efa7ff77408e9e80104aa3133e170d63074
Roshan Thaabith <roshlarosh@gmail.com>
2020-02-19 23:21:27 +0000

    fix the about me page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import "./AboutPage.css";
-import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const initialState = {
   Name: "",
@@ -15,7 +16,17 @@ function reducer(state, { field, value }) {
   };
 }
 
+const useStyles = makeStyles({
+  button: {
+    width: "30px",
+    height: "50px",
+    backgroundColor: "#f5f5ef",
+    fontSize: "15px"
+  }
+});
+
 const AboutPage = () => {
+  const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, initialState);
   // const [changePage, setChangePage] = React.useState(false);
 
@@ -27,87 +38,90 @@ const AboutPage = () => {
 
   return (
     <div>
-      <h1 className="headline">About me</h1>
+      <h1 className="headline-about">About me</h1>
       <hr className="underline" />
-      <p className="bio">
-        I am a <b>software developer</b> at Founders & Coders with a background
-        in software testing. I enjoy writing <b>clean, concise code</b>, and{" "}
-        <b>building intuitive UI</b>. I am looking for a front-end/full-stack
-        role in a fast-paced forward-thinking company with a{" "}
-        <b>supportive culture</b> that values
-        <b>diversity</b>, where I can <b>learn</b> and grow to be the best web
-        software developer I can be.
-      </p>
-      <button>
-        <a
-          class=""
-          href="../../../public/asset/roshan_thaabith_cv copy.pdf"
-          downdload
-          target="_blank"
-        >
-          {" "}
-          CV↓{" "}
-        </a>
-      </button>
-      <div className="form-container">
-        <form
-          action="https://formspree.io/xlebobod"
-          method="POST"
-          className="form"
-        >
-          <h1 className="headline">Contact me</h1>
-          <hr className="underline" />
-          <fieldset className="fieldset">
-            <div>
-              <label htmlFor="Name">Name:</label>
+      <section className="bio-form">
+        <p className="bio">
+          I am a <b>software developer</b> at Founders & Coders with a
+          background in software testing. I enjoy writing{" "}
+          <b>clean, concise code</b>, and <b>building intuitive UI</b>. I am
+          looking for a front-end/full-stack role in a fast-paced
+          forward-thinking company with a <b>supportive culture</b> that values
+          <b>diversity</b>, where I can <b>learn</b> and grow to be the best web
+          software developer I can be.
+        </p>
+        <div className="form-container">
+          <form
+            action="https://formspree.io/xlebobod"
+            method="POST"
+            className="form"
+          >
+            {/* <h1 className="headline">Contact me</h1>
+            <hr className="underline" /> */}
+            <fieldset className="fieldset">
+              <div>
+                <label htmlFor="Name">Name:</label>
+                <br />
+                <input
+                  type="text"
+                  name="Name"
+                  className="input-field"
+                  id="Name"
+                  value={Name}
+                  onChange={onChange}
+                  required
+                />
+              </div>
               <br />
-              <input
-                type="text"
-                name="Name"
-                className="input-field"
-                id="Name"
-                value={Name}
-                onChange={onChange}
-                required
-              />
-            </div>
-            <br />
-            <div>
-              <label htmlFor="Email">Email:</label>
+              <div>
+                <label htmlFor="Email">Email:</label>
+                <br />
+                <input
+                  type="Email"
+                  name="Email"
+                  id=""
+                  className="input-field"
+                  value={Email}
+                  onChange={onChange}
+                  required
+                />
+              </div>
               <br />
-              <input
-                type="Email"
-                name="Email"
-                id=""
-                className="input-field"
-                value={Email}
-                onChange={onChange}
-                required
-              />
-            </div>
-            <br />
-            <div>
-              <label htmlFor="Message">Message:</label>
+              <div>
+                <label htmlFor="Message">Message:</label>
+                <br />
+                <textarea
+                  type="text"
+                  name="Message"
+                  id="messageBox"
+                  wra="soft"
+                  className="input-field"
+                  value={Message}
+                  onChange={onChange}
+                />
+              </div>
               <br />
-              <textarea
-                type="text"
-                name="Message"
-                id="messageBox"
-                wra="soft"
-                className="input-field"
-                value={Message}
-                onChange={onChange}
-              />
-            </div>
-            <br />
-            <button type="submit" className="submit-button">
-              Send
-            </button>
-          </fieldset>
-        </form>
-      </div>
+              <Button type="submit" className={classes.button}>
+                Send
+              </Button>
+            </fieldset>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
 
 export default AboutPage;
+
+// <button>
+// <a
+//   class=""
+//   href="../../../public/asset/roshan_thaabith_cv copy.pdf"
+//   downdload
+//   target="_blank"
+// >
+//   {" "}
+//   CV↓{" "}
+// </a>
+// </button>
